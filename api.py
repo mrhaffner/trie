8ba60b2c88078685259b8b290e6fb3b8b20b0370
mrhaffner <mrhaffner13@gmail.com>
2022-07-31 19:47:56 -0500

    added post route to trie

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
+import json
 from flask import Flask, jsonify, request
-# from requests import request
 from trie import Trie
 
 t = Trie()
@@ -17,14 +17,20 @@ def index():
     return "Welcome to the API."
 
 
-@app.route("/trie")
-def trie():
+@app.route("/trie", methods=["GET"])
+def query_trie():
     prefix = request.args.get("prefix")
-    number = int(request.args.get("number"))
+    number = request.args.get("number")
     suggestions = t.get_suggestions(prefix)
-    output = suggestions[:number] if number else suggestions
+    output = suggestions[:int(number)] if number else suggestions
 
     return jsonify(output)
 
+
+@app.route("/trie", methods=["POST"])
+def add_word():
+    word = json.loads(request.data)
+    t.add(word)
+    return "", 200
 
 app.run()

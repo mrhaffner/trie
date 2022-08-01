8ab875aae8371a12922c80d3c0419dbdbb41a924
mrhaffner <mrhaffner13@gmail.com>
2022-08-01 14:59:10 -0500

    input now has on change listener to dynamically update search suggestions

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const testArr = ['Apple', 'AppleOrchard', 'Apple Orchard'];
-const testUri = 'http://127.0.0.1:5000/trie?prefix=App';
+const testUri = 'http://127.0.0.1:5000/trie?prefix=';
 
 const headers = {};
-
-let suggestionsParent = document.getElementById('suggestions');
 
 const arrayToSuggestionsLIs = (suggestions) => {
   return suggestions.map((suggestion) => {
@@ -14,10 +12,18 @@ const arrayToSuggestionsLIs = (suggestions) => {
   });
 };
 
+let input = document.getElementById('search-input');
+let suggestionsParent = document.getElementById('suggestions');
+
 // needs to handle error
-fetch(testUri, headers)
-  .then((response) => response.json())
-  .then((suggestions) => arrayToSuggestionsLIs(suggestions))
-  .then((nodes) =>
-    nodes.forEach((node) => suggestionsParent.appendChild(node)),
-  );
+const updateSuggestions = (e) => {
+  suggestionsParent.firstChild?.remove();
+  let ul = document.createElement('ul');
+  suggestionsParent.append(ul);
+  fetch(`${testUri}${e.target.value}`)
+    .then((response) => response.json())
+    .then((suggestions) => arrayToSuggestionsLIs(suggestions))
+    .then((nodes) => nodes.forEach((node) => ul.appendChild(node)));
+};
+
+input.addEventListener('change', updateSuggestions);

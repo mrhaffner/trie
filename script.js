3b418921b5a56fb473ea65fb53f8d03f8201750f
mrhaffner <mrhaffner13@gmail.com>
2022-08-01 15:34:00 -0500

    refactored to use async/awaits. search suggestion box is now hidden when there are no suggestions

--- script.js
+++ script.js
@@ -15,17 +15,30 @@ const arrayToSuggestionsLIs = (suggestions) => {
 let input = document.getElementById('search-input');
 let suggestionsParent = document.getElementById('suggestions');
 
-// needs to handle error
-const updateSuggestions = (e) => {
-  suggestionsParent.firstChild?.remove();
-  if (e.target.value == '') return;
+// debounce
+// should not make requests if there are character(s) and already no suggestions
+// should show itself as a suggestion if it is there? probably on back end
 
+const updateSuggestions = async (e) => {
+  if (e.target.value == '') {
+    suggestionsParent.hidden = true;
+    return;
+  }
+
+  const response = await fetch(`${testUri}${e.target.value}`);
+  const suggestions = await response.json();
+
+  if (!suggestions.length) {
+    suggestionsParent.hidden = true;
+    return;
+  }
+
+  suggestionsParent.firstChild?.remove();
   let ul = document.createElement('ul');
   suggestionsParent.append(ul);
-  fetch(`${testUri}${e.target.value}`)
-    .then((response) => response.json())
-    .then((suggestions) => arrayToSuggestionsLIs(suggestions))
-    .then((nodes) => nodes.forEach((node) => ul.appendChild(node)));
+
+  arrayToSuggestionsLIs(suggestions).forEach((node) => ul.appendChild(node));
+  suggestionsParent.hidden = false;
 };
 
 input.addEventListener('input', updateSuggestions);

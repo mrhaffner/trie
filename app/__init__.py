e8d50847d5eb22179b26dfa942ee4bf09813fd97
mrhaffner <mrhaffner13@gmail.com>
2022-08-14 12:03:38 -0500

    updated jest testing setup, added jquery, removed typescript, javascript now adds event listener + tests

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,12 @@ from trie.standard_trie import StandardTrie
 
 trie = StandardTrie(200)
 
+# for testing
+starting_suffixes = ["app", "apple", "apple orchard", "dog"] 
+starting_suffixes += ["z" * i for i in range(2, 16)]
+for suffix in starting_suffixes:
+    trie.insert(suffix)
+
 def create_app():
     app = Flask(__name__)
 
@@ -12,3 +18,4 @@ def create_app():
     app.register_blueprint(bp)
 
     return app
+
---
+++ app/static/script.js
@@ -0,0 +1,30 @@
+// const $ = require('jquery');
+
+('use strict');
+
+const testUri = 'http://127.0.0.1:5000/api/trie/suggestions?prefix=';
+
+main();
+
+function main() {
+  $('#trie-search-bar').on('click', () => {
+    $('#trie-search-bar').addClass('search-bar-active');
+  });
+
+  // $('#search-input').on('input', () => {});
+}
+
+// test that main is called?
+
+async function getSuggestions(prefix, uri) {
+  try {
+    const response = await fetch(`${uri}${encodeURI(prefix)}`);
+    const suffixes = await response.json();
+    return suffixes.map((suffix) => prefix + suffix);
+  } catch (e) {
+    console.error(e, e.stack);
+    return [];
+  }
+}
+
+// module.exports = { getSuggestions, main };
